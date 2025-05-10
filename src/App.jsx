16db84ea5dd04a29f51fd2bec80e72ebdb71e560
otiko1o <otikogrigoliaa121@gmail.com>
2025-05-10 18:22:10 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  let username = 'giorgi'
+  let age = 16
+
+  return (
+   <div>
+    <h1>hello world</h1>
+    <h2 className='blue'>my name is {username}</h2>
+    <h2>i am {age} years old</h2>
+   </div>
+  )
+}
+
+export default App
